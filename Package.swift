c9fb07bca30dbcc37e1e1294fc5be2c1731436e7
Ben Szymanski <bszyman@icloud.com>
2026-01-07 17:39:55 -0600

    Update config to point to release 1.0.0 binary

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperGooeyMobile",
-            url: "",
-            checksum: ""
+            url: "https://github.com/bszyman/hypergooey-mobile-dist/releases/download/1.0.0/HyperGooeyMobile.xcframework.zip",
+            checksum: "8a7c38c8313b5c866a34424f660526de53eae9037897afb781ffcc8dfb8a1790"
         )
     ]
 )
